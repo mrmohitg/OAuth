1be7fc8defdbfdf519618447dd8695ac536dae05
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-03-15 20:49:40 +0530

    Create WebSecurityConfiguration, UserDetailsService and Expose AuthenticationManager Bean

---
+++ oauthdemo/src/main/java/com/training/oauth/config/CustomeUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.training.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.training.oauth.data.entities.User;
+import com.training.oauth.data.repo.UserRepository;
+
+public class CustomeUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByName(username);
+		return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(),
+				user.getRoles());
+	}
+
+}
