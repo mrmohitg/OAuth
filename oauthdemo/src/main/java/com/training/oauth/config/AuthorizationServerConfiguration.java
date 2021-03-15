f4d1a1beae5d476e33f6311c4d0adc5aca7373ff
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-03-15 21:17:24 +0530

    CreateExposeTokenServicesBean & Configure Client Information

--- oauthdemo/src/main/java/com/training/oauth/config/AuthorizationServerConfiguration.java
+++ oauthdemo/src/main/java/com/training/oauth/config/AuthorizationServerConfiguration.java
@@ -2,12 +2,16 @@ package com.training.oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
@@ -29,6 +33,20 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 		endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager)
 				.userDetailsService(userDetailsService);
 
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("myclientapp").authorizedGrantTypes("password","referesh_token").scopes("read","write").secret("9999");
+		super.configure(clients);
+	}
+	
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices tokenServices = new DefaultTokenServices();
+		tokenServices.setTokenStore(tokenStore);
+		return tokenServices;
 	}
 
 }
