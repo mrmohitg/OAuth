84d88f7d826e6c7016fbadc67df24b3bb3115a57
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-03-17 19:14:57 +0530

    Bug Fix

--- oauthdemo/src/main/java/com/training/oauth/services/CustomUserDetailsService.java
+++ oauthdemo/src/main/java/com/training/oauth/services/CustomUserDetailsService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.training.oauth.data.entities.User;
 import com.training.oauth.data.repo.UserRepository;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
